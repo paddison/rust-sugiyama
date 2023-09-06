34a7977eb9af4ba8168c49e73f0d504329d67a05
paddison <patrickbaumann579@gmail.com>
2023-09-06 15:24:47 +0200

    update build_layout to take vertex spacing, change visibility functions in algorithm.rs

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -4,22 +4,22 @@ use petgraph::stable_graph::NodeIndex;
 
 use crate::phases::{p3_calculate_coordinates::{MinimalCrossings, VDir, HDir}, p1_layering::start, p2_reduce_crossings::InsertDummyVertices};
 
-pub fn build_layout(edges: &[(u32, u32)], minimum_length: u32) -> Vec<(usize, (isize, isize))> {
+pub fn build_layout(edges: &[(u32, u32)], minimum_length: u32, vertex_spacing: usize) -> Vec<(usize, (isize, isize))> {
     let proper_graph = rank(edges, minimum_length);
     let minimal_crossings = minimize_crossings(proper_graph);
-    calculate_coordinates(minimal_crossings, 10)
+    calculate_coordinates(minimal_crossings, vertex_spacing)
 }
 
-pub fn rank(edges: &[(u32, u32)], minimum_length: u32) -> InsertDummyVertices {
+fn rank(edges: &[(u32, u32)], minimum_length: u32) -> InsertDummyVertices {
     start(edges, minimum_length).init_rank().make_tight().init_cutvalues().init_low_lim().rank().into()
 }
 
-pub fn minimize_crossings(graph: InsertDummyVertices) -> MinimalCrossings {
+fn minimize_crossings(graph: InsertDummyVertices) -> MinimalCrossings {
     graph.prepare_for_initial_ordering().ordering()
 }
 
 /// Calculates the final x-coordinates for each vertex, after the graph was layered and crossings where minimized.
-pub fn calculate_coordinates(graph: MinimalCrossings, vertex_spacing: usize) -> Vec<(usize, (isize, isize))>{
+fn calculate_coordinates(graph: MinimalCrossings, vertex_spacing: usize) -> Vec<(usize, (isize, isize))>{
     let y_coordinates = graph.layers.iter()
         .enumerate()
         .map(|(rank, row)| row.iter().map(move |v| (*v, rank as isize * vertex_spacing as isize)))
@@ -102,7 +102,7 @@ mod benchmark {
     fn r_100() {
         let edges = graph_generator::RandomLayout::new(100).build_edges().into_iter().map(|(r, l)| (r as u32, l as u32)).collect::<Vec<(u32, u32)>>();
         let start = std::time::Instant::now();
-        let _ = build_layout(&edges, 1);
+        let _ = build_layout(&edges, 1, 10);
         println!("Random 100 edges: {}ms", start.elapsed().as_millis());
     }
 
@@ -110,7 +110,7 @@ mod benchmark {
     fn r_1000() {
         let edges = graph_generator::RandomLayout::new(1000).build_edges().into_iter().map(|(r, l)| (r as u32, l as u32)).collect::<Vec<(u32, u32)>>();
         let start = std::time::Instant::now();
-        let _ = build_layout(&edges, 1);
+        let _ = build_layout(&edges, 1, 10);
         println!("Random 1000 edges: {}ms", start.elapsed().as_millis());
     }
 
@@ -118,7 +118,7 @@ mod benchmark {
     fn r_2000() {
         let edges = graph_generator::RandomLayout::new(2000).build_edges();
         let start = std::time::Instant::now();
-        let _ = build_layout(&edges, 1);
+        let _ = build_layout(&edges, 1, 10);
         println!("Random 2000 edges: {}ms", start.elapsed().as_millis());
     }
 
@@ -126,14 +126,14 @@ mod benchmark {
     fn r_4000() {
         let edges = graph_generator::RandomLayout::new(2000).build_edges();
         let start = std::time::Instant::now();
-        let _ = build_layout(&edges, 1);
+        let _ = build_layout(&edges, 1, 10);
         println!("Random 4000 edges: {}ms", start.elapsed().as_millis());
     }
     #[test]
     fn r_8000() {
         let edges = graph_generator::RandomLayout::new(8000).build_edges();
         let start = std::time::Instant::now();
-        let _ = build_layout(&edges, 1);
+        let _ = build_layout(&edges, 1, 10);
         println!("Random 8000 edges: {}ms", start.elapsed().as_millis());
     }
 
@@ -143,7 +143,7 @@ mod benchmark {
         let e = 2;
         let edges = graph_generator::GraphLayout::new_from_num_nodes(n, e).build_edges();
         let start = std::time::Instant::now();
-        let _ = build_layout(&edges, 1);
+        let _ = build_layout(&edges, 1, 10);
         println!("{n} nodes, {e} edges per node: {}ms", start.elapsed().as_millis());
     }
 }
@@ -161,7 +161,7 @@ mod check_visuals {
                 (3, 5), (3, 6), (3, 7), (3, 8), (4, 5), (4, 6), (4, 7), (4, 8),
                 (5, 9), (6, 9), (7, 9), (8, 9)
         ];
-        let layout = build_layout(&edges, 1); 
+        let layout = build_layout(&edges, 1, 10); 
         println!("{:?}", layout);
     }
 
