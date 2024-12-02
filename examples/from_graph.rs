7d817cdf633576cfa3d13265d5e275f510d9491d
andriyDev <andriydzikh@gmail.com>
2024-12-01 23:04:16 -0800

    Render the layout in from_graph as an SVG.
    Co-authored-by: dolezvo1 <dolezvo1@users.noreply.github.com>

--- examples/from_graph.rs
+++ examples/from_graph.rs
@@ -1,6 +1,17 @@
-use petgraph::stable_graph::{NodeIndex, StableDiGraph};
+use petgraph::{
+    stable_graph::{NodeIndex, StableDiGraph},
+    visit::EdgeRef,
+    Direction,
+};
 use rust_sugiyama::from_graph;
 use std::collections::HashMap;
+use svg::{
+    node::{
+        element::{Line, Rectangle, Text},
+        Comment,
+    },
+    Document,
+};
 
 fn main() {
     let mut g: StableDiGraph<String, usize> = StableDiGraph::new();
@@ -19,9 +30,12 @@ fn main() {
     g.add_edge(jerry, morty, 1);
 
     let layouts = from_graph(&g)
+        .vertex_spacing(100)
         .build()
         .into_iter()
         .map(|(layout, width, height)| {
+            println!("{}", create_svg(&g, &layout, 15.0, 25.0, (10.0, 10.0)));
+
             let mut new_layout = HashMap::new();
             for (id, coords) in layout {
                 new_layout.insert(g[NodeIndex::from(id)].clone(), coords);
@@ -34,4 +48,93 @@ fn main() {
         println!("Coordinates: {:?}", layout);
         println!("width: {width}, height: {height}");
     }
+}
+
+/// Generates an SVG string of the resulting layout. This is an example of
+/// rendering the layouts.
+fn create_svg(
+    graph: &StableDiGraph<String, usize>,
+    layout: &[(NodeIndex, (isize, isize))],
+    character_width: f32,
+    node_height: f32,
+    padding: (f32, f32),
+) -> String {
+    // Figure out the extents of the SVG.
+    let node_size = |idx: NodeIndex| {
+        (
+            graph.node_weight(idx).unwrap().len() as f32 * character_width,
+            node_height,
+        )
+    };
+    let f32_cmp = |a: &f32, b: &f32| a.partial_cmp(b).unwrap();
+    let min_x = layout
+        .iter()
+        .map(|(idx, (x, _))| *x as f32 - node_size(*idx).0 * 0.5)
+        .min_by(f32_cmp)
+        .unwrap();
+    let max_x = layout
+        .iter()
+        .map(|(idx, (x, _))| *x as f32 + node_size(*idx).0 * 0.5)
+        .max_by(f32_cmp)
+        .unwrap();
+    let min_y = layout
+        .iter()
+        .map(|(idx, (_, y))| *y as f32 - node_size(*idx).1 * 0.5)
+        .min_by(f32_cmp)
+        .unwrap();
+    let max_y = layout
+        .iter()
+        .map(|(idx, (_, y))| *y as f32 + node_size(*idx).1 * 0.5)
+        .max_by(f32_cmp)
+        .unwrap();
+
+    let mut document = Document::new()
+        .set("width", (max_x - min_x) as f32 + 2.0 * padding.0)
+        .set("height", (max_y - min_y) as f32 + 2.0 * padding.1);
+
+    let origin = (padding.0 - min_x as f32, padding.1 - min_y as f32);
+
+    // Generate a line for each edge in the graph.
+    for (idx, pos) in layout {
+        let vertex_name = graph.node_weight(*idx).unwrap();
+        for e in graph.edges_directed(*idx, Direction::Outgoing) {
+            let other_vertex_index = e.target();
+            let other_vertex_name = graph.node_weight(other_vertex_index).unwrap();
+            let (_, other_vertex_pos) = layout.iter().find(|e| e.0 == other_vertex_index).unwrap();
+
+            let line = Line::new()
+                .set("x1", pos.0 as f32 + origin.0)
+                .set("y1", pos.1 as f32 + origin.1)
+                .set("x2", other_vertex_pos.0 as f32 + origin.0)
+                .set("y2", other_vertex_pos.1 as f32 + origin.1)
+                .set("style", "stroke:black;")
+                .add(Comment::new(format!(
+                    "({vertex_name}, {other_vertex_name})"
+                )));
+            document = document.add(line);
+        }
+    }
+
+    // Generate a rectangle for each edge in the graph.
+    for (idx, pos) in layout {
+        let vertex_name = graph.node_weight(*idx).unwrap();
+
+        let size = (vertex_name.len() as f32 * character_width, node_height);
+
+        let rect = Rectangle::new()
+            .set("x", pos.0 as f32 - size.0 * 0.5 + origin.0)
+            .set("y", pos.1 as f32 - size.1 * 0.5 + origin.1)
+            .set("width", size.0)
+            .set("height", size.1)
+            .set("style", "fill:white;stroke:black;");
+
+        let text = Text::new(vertex_name)
+            .set("x", pos.0 as f32 + origin.0)
+            .set("y", pos.1 as f32 + origin.1)
+            .set("dominant-baseline", "middle")
+            .set("text-anchor", "middle");
+        document = document.add(rect).add(text);
+    }
+
+    document.to_string()
 }
